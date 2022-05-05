275dd32ff07f496d704483f5a3018e3e89b43bfa
Fridman Ilya <103580496+fridmanthesemite@users.noreply.github.com>
2022-05-05 18:11:50 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -17,7 +17,7 @@ class InfoMessage:
         pass
 
     def get_message(self) -> str:
-        return (f" Тип тренировки: {self.training_type};"
+        return (f"Тип тренировки: {self.training_type};"
                 f" Длительность: {self.duration:.3f} ч.;"
                 f" Дистанция: {self.distance:.3f} км;"
                 f" Ср. скорость: {self.speed:.3f} км/ч;"
